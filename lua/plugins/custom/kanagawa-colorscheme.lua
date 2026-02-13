f7de692e96b3490fba5cd3f4f950a6d722f5ecbb
Brandon Mach <brandonmachh@gmail.com>
2026-02-12 21:18:31 -0800

    update kanagawa colorscheme configuration

--- lua/plugins/custom/kanagawa-colorscheme.lua
+++ lua/plugins/custom/kanagawa-colorscheme.lua
@@ -3,6 +3,44 @@
 
 return {
   'rebelot/kanagawa.nvim',
-  lazy = false,
   priority = 1000,
+  config = function()
+    require('kanagawa').setup {
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = {
+        italic = true,
+      },
+      functionStyle = {},
+      keywordStyle = {
+        italic = true,
+      },
+      statementStyle = {
+        bold = true,
+      },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window ':hl-NormalNC'
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = {
+        palette = {},
+        theme = {
+          wave = {},
+          lotus = {},
+          dragon = {},
+          all = {},
+        },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = 'wave',
+      background = {
+        dark = 'wave',
+        light = 'lotus',
+      },
+    }
+
+    vim.cmd 'colorscheme kanagawa'
+  end,
 }
